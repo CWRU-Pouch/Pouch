df7faf9d18819bba3de42e30c78e04c6e99a419c
John <thehumantorch88@gmail.com>
2018-05-15 19:53:33 -0400

    handlebars files

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
+//adding express-handlebars
+var exphbs = require("express-handlebars");
+
 var app = express();
 var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 app.use(express.static("app/public"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 require("./app/routes/api-routes.js")(app)
 
